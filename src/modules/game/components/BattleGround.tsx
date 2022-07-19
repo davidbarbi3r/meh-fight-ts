90b11399439a0d7e1114093a8c351e23305cbea4
davidbarbi3r <davidbarbi3r@gmail.com>
2022-07-19 14:25:21 +0200

    :lipstick: Changed some styling

--- src/modules/game/components/BattleGround.tsx
+++ src/modules/game/components/BattleGround.tsx
@@ -1,24 +1,33 @@
 import Header from "../../layout/components/Header";
 import Footer from "../../layout/components/Footer";
-import {Cards} from "./Cards";
+import { Cards } from "./Cards";
 import Hero from "./Hero";
 import Enemy from "./Enemy";
 import { CardModel, EnemyModel, HeroModel } from "../../data/Data";
 
 interface BattleGroundProps {
-    resetGame: () => void,
-    heroSelected: HeroModel,
-    currentEnemy: EnemyModel,
-    hand: CardModel[],
-    useCard: (card: CardModel) => void,
-    endTurn: () => void,
-    discardPile: number,
-    deck: number
+  resetGame: () => void;
+  heroSelected: HeroModel;
+  currentEnemy: EnemyModel;
+  hand: CardModel[];
+  useCard: (card: CardModel) => void;
+  endTurn: () => void;
+  discardPile: number;
+  deck: number;
 }
 
-function BattleGround({resetGame, heroSelected, currentEnemy, hand, useCard, endTurn, discardPile, deck}:BattleGroundProps) {
+function BattleGround({
+  resetGame,
+  heroSelected,
+  currentEnemy,
+  hand,
+  useCard,
+  endTurn,
+  discardPile,
+  deck,
+}: BattleGroundProps) {
   return (
-    <div className="game-content">
+    <div className="App">
       <Header resetGame={resetGame} />
       <section className="App-game-container">
         <div className="App-game-players-container">
@@ -36,7 +45,7 @@ function BattleGround({resetGame, heroSelected, currentEnemy, hand, useCard, end
       </section>
       <p>Discard pile size: {discardPile}</p>
       <p>Deck size: {deck}</p>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
